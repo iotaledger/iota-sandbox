41f5034692a1c7248205b156fae81acc5455de89
Dr-Electron <dr-electr0n@protonmail.com>
2023-11-14 21:41:05 +0100

    Adapt config

--- sandbox/bootstrap.sh
+++ sandbox/bootstrap.sh
@@ -6,50 +6,38 @@ if [[ "$OSTYPE" != "darwin"* && "$EUID" -ne 0 ]]; then
 fi
 
 # Cleanup if necessary
-if [ -d "sandboxdb" ] || [ -d "snapshots" ]; then
-  ./cleanup.sh
+if [ -d "data" ]; then
+  docker compose down --remove-orphans
+  rm -Rf data
 fi
 
-if [[ $1 = "build" ]]; then
-  # Build latest code
-  docker compose --profile "bootstrap" build
+# Prepare db directory
+mkdir -p data/grafana
+mkdir -p data/prometheus
+mkdir -p data/sandboxdb/dashboard
+mkdir -p data/sandboxdb/database_legacy
+mkdir -p data/sandboxdb/database_chrysalis
+mkdir -p data/sandboxdb/wasp
+mkdir -p data/sandboxdb/hornet
+mkdir -p data/sandboxdb/hornet-1
+mkdir -p data/sandboxdb/state
+mkdir -p data/sandboxdb/indexer
+mkdir -p data/sandboxdb/participation
+mkdir -p data/snapshots/hornet
 
-  # Pull latest images
-  docker compose pull inx-coordinator
-  docker compose pull inx-indexer
-  docker compose pull inx-mqtt
-  docker compose pull inx-faucet
-  docker compose pull inx-participation
-  docker compose pull inx-spammer
-  docker compose pull inx-poi
-  docker compose pull inx-dashboard
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  chown -R 65532:65532 data
 fi
 
 # Create snapshot
-mkdir -p snapshots/hornet
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  chown -R 65532:65532 snapshots
-fi
 docker compose run create-snapshots
 
-# Prepare database directory for hornet-1
-mkdir -p sandboxdb/hornet
-mkdir -p sandboxdb/state
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  chown -R 65532:65532 sandboxdb
-fi
-
-# Bootstrap network (create hornet-1 database, create genesis milestone, create coo state)
+# Bootstrap network (create hornet database, create genesis milestone, create coo state)
 docker compose run bootstrap-network
 
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  chown -R 65532:65532 snapshots
-fi
+# Duplicate snapshot
+cp -R data/snapshots/hornet data/snapshots/hornet-1
 
-# Prepare database directory
-mkdir -p sandboxdb/indexer
-mkdir -p sandboxdb/participation
-#mkdir -p sandboxdb/hornet
 if [[ "$OSTYPE" != "darwin"* ]]; then
-  chown -R 65532:65532 sandboxdb
+  chown -R 65532:65532 data
 fi
